e2e4331c10e8ba9b88ccfc1fb51b023edaf76c20
Rajupasunuri <rajupasunuri1999@gmail.com>
2024-01-11 23:46:56 +0530

    child commit

--- src/pages/Apps/Child.tsx
+++ src/pages/Apps/Child.tsx
@@ -1,7 +1,10 @@
 const Child = () => {
     return (
         <div>
-            <h2>HEllo Child page</h2>
+            <div className="pb-5 text-lg text-black font-bold">MI Of Your Child</div>
+            <div className="panel h-18">
+                <h2 className="text-base">NOTE: Date expired, Please contact to admin for the details.</h2>
+            </div>
         </div>
     );
 };
